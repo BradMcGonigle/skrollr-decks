3ae6ba89dcc27a1e2d472f8309f5928957696f86
Brad McGonigle <brad@bradmcgonigle.com>
2014-10-02 17:08:48 -0400

    removing deck animation because it sucks for usability

--- src/skrollr.decks.js
+++ src/skrollr.decks.js
@@ -13,12 +13,12 @@
 	var skrollr = window.skrollr,
 		setTimeout = window.setTimeout,
 		clearTimeout = window.clearTimeout;
-	
+
 	var DEFAULT_SEGMENT = 'skrollr-decks-segment',
 		DEFAULT_NAV     = 'skrollr-decks-nav';
-	
+
 	var _lastOffset;
-	
+
 
 	function setHeight(segments) {
 		var height = window.innerHeight + 2 + 'px',
@@ -28,13 +28,13 @@
 			segments[i].style.minHeight = height;
 		}
 	}
-	
+
 	function resizeSegments(segmentClass) {
 		var segments = document.getElementsByClassName(segmentClass);
-		
+
 		setHeight.call(window, segments);
 		window.addEventListener('resize', setHeight.bind(window, segments), false);
-		
+
 		return segments;
 	}
 
@@ -43,7 +43,7 @@
 		var menu = document.createElement('ul'),
 			item = document.createElement('li'),
 			i, max, newItem;
-		
+
 		menu.className = navClass;
 		item.setAttribute('data-top-bottom', '');
 		item.setAttribute('data-bottom-top', '');
@@ -55,20 +55,20 @@
 		}
 
 		document.body.appendChild(menu);
-		
+
 		return menu;
 	}
 
-	
+
 	function findByClass(parent, className, up) {
 		var items = parent.getElementsByClassName(className);
-		
+
 		return items[up ? 0 : items.length - 1];
 	}
-	
+
 	function getById(items, id) {
 		var el, i;
-		
+
 		for(i = items.length; i--; ) {
 			el = items[i];
 			if(el.id === id) {
@@ -87,27 +87,15 @@
 			target = getById(segments, item.getAttribute('data-anchor-target').substr(1));
 			offset = scroller.relativeToAbsolute(target, 'top', 'top');
 		}
-		
-		if( ! scroller.isAnimatingTo() && offset !== _lastOffset) {
-			scroller.animateTo(offset + 1, {
-				duration: 300,
-				done: function () {
-					var that = this;
-					setTimeout(function () {
-						that.stopAnimateTo();
-					}, 20);
-				}
-			});
-		}
 
 		_lastOffset = offset;
 	}
 
-	
+
 	skrollr.decks = {
 		init: function (options) {
 			options = options || {};
-			
+
 			var segments = resizeSegments(options.segment || DEFAULT_SEGMENT),
 				nav = generateNav(segments, options.nav || DEFAULT_NAV),
 				_renderTimer;
@@ -115,10 +103,10 @@
 			this._instance = skrollr.init({
 				forceHeight: false
 			});
-			
-			
+
+
 			this._instance.refresh();
-			
+
 			this._instance.on('render', function (options) {
 					var scroller = this;
 					clearTimeout(_renderTimer);
